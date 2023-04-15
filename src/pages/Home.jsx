ee2979482fbe1190f3959a56836e7e0e83b733c9
Kimyebin00 <kimyeobin12@gmail.com>
2023-04-15 10:35:14 +0900

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+function Home() {
+  return (
+    <Container>
+      홈입니다.
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  background: skyblue;
+  color: white;
+  padding: 400px;
+`
+
+export default Home;
---
+++ src/pages/Movie.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+function Movie() {
+  return ( 
+    <Container>
+      무비컨텐츠
+    </Container>
+  );
+}
+
+// styled component
+const Container = styled.div`
+  background: grey;
+  color: white;
+  padding: 400px;
+`
+export default Movie;
